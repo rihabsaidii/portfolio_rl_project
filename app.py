964ce8e3add1856d086f9fc5c0dea06170ff78f9
Rihab Saidi <rihab@laevitas.ch>
2025-01-24 16:15:41 +0100

    procfile

--- app.py
+++ app.py
@@ -218,6 +218,6 @@ def calculate_metrics(selected_stocks):
 
 if __name__ == '__main__':
     # app.run_server(debug=True)
-    serve(app, host="0.0.0.0", port=8050)
+    serve(app.server, host="0.0.0.0", port=8050)
 
     # app.run_server(debug=True, host="0.0.0.0", port=8050)
